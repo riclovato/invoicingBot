2ae21c3db09a11905dc1b10d95422b818b77df0c
riclovato <riic.lovato@gmail.com>
2024-11-04 11:20:00 -0300

    validação estrutura do arquivo

--- app.py
+++ app.py
@@ -19,6 +19,23 @@ class InvoicingBot:
     def read_file(self, file_path):
         try:
             df = pd.read_excel(file_path)
+
+            if 'date' not in df.columns.str.lower() and 'data'not in df.columns.str.lower():
+                print("O arquivo não possui a coluna 'Data'")
+                return None
+            if 'account' not in df.columns.str.lower() and 'conta' not in df.columns.str.lower():
+                print("O arquivo não possui a coluna 'Conta'")
+                return None
+            if 'contact' not in df.columns.str.lower() and 'contato' not in df.columns.str.lower():
+                print("O arquivo não possui a coluna 'Contato'")
+                return None
+            if 'amount' not in df.columns.str.lower() and 'valor' not in df.columns.str.lower():
+                print("O arquivo não possui a coluna 'Valor'")
+                return None
+            if 'Status' not in df.columns.str.lower():
+                print("O arquivo não possui a coluna 'Status'")
+                return None
+
             print(df)
         except Exception as e:
             print(f"Erro ao ler o arquivo: {e}")
